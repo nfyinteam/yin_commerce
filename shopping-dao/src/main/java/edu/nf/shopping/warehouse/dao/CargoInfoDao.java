70b18de60a35ac5b39055dee5b521655f37560db
lishun <2537984209@qq.com>
2020-03-14 22:47:54 +0800

    供应商、货物Mapper

--- shopping-dao/src/main/java/edu/nf/shopping/warehouse/dao/CargoInfoDao.java
+++ shopping-dao/src/main/java/edu/nf/shopping/warehouse/dao/CargoInfoDao.java
@@ -10,8 +10,25 @@ import java.util.List;
  * @date 2020/3/11
  */
 public interface CargoInfoDao {
+    /**
+     *查询所有货物信息
+     * @return
+     */
     List<CargoInfo> listCargoInfo();
-    CargoInfo getCargoInfoById(String id);
-    void updateCargoIsOutById(String id,Boolean isOut);
+
+    /**
+     * 添加货物信息
+     */
+    void insertCargoInfo();
+
+    /**
+     * 删除货物信息
+     */
+    void deleteCargoInfo();
+
+    /**
+     * 修改货物信息
+     */
+    void updateCargoInfo();
 
 }
