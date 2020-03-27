9497db1e5d3d8e3ea330660fe76477d306e7b8e8
achine <a291774405@vip.qq.com>
2020-03-27 21:23:10 +0800

    update UserInfo

--- shopping-userapi/src/main/java/edu/nf/shopping/user/controller/UserInfoController.java
+++ shopping-userapi/src/main/java/edu/nf/shopping/user/controller/UserInfoController.java
@@ -1,14 +1,11 @@
 package edu.nf.shopping.user.controller;
-
-import com.example.project.controller.vo.ResponseVO;
-import com.example.project.servise.UserService;
 import edu.nf.shopping.user.entity.UserInfo;
 import edu.nf.shopping.user.service.UserInfoService;
 import edu.nf.shopping.vo.BaseController;
 import edu.nf.shopping.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.security.auth.login.LoginException;
 import javax.validation.Valid;
@@ -18,7 +15,7 @@ import javax.validation.Valid;
  * @date 2020/3/22
  */
 @RestController
-public class UserinfoController extends BaseController {
+public class UserInfoController extends BaseController {
     @Autowired
     private UserInfoService service;
 
